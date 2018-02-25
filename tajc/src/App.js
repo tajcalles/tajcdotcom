c55401bcb32981eaa812a8082513cff88b0f974d
Taj Calles <tajcalles@gmail.com>
2018-02-25 01:05:59 -0700

    init commit

---
+++ tajc/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Segment, 
+  Header, 
+  Column, 
+  Row, 
+  Divider 
+} from 'semantic-ui-react';
+
+class App extends Component {
+  render() {
+    return (
+    <Segment>
+      <Header as="h1">Taj Calles</Header>
+    </Segment>
+    );
+  }
+}
+
+export default App;
